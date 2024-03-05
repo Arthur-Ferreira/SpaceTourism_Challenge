eb9813e13ddec513dfb1cf6992e5b2645c80b0c4
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-05 17:31:31 -0300

    Feat Destination: Finish Destination Template

--- src/components/Templates/Destination/index.tsx
+++ src/components/Templates/Destination/index.tsx
@@ -1,32 +1,54 @@
-import { H2, H5 } from "../../Atoms";
-import { StlSection, SectionContainer } from "./styles";
+import { H2, H5, SubH1, SubH2 } from "../../Atoms";
+import {
+  StlSection,
+  SectionContainer,
+  ContainerHeader,
+  HeaderImage,
+  ContainerBody,
+  SubHeading,
+} from "./styles";
+import moon from "../../../assets/destination/image-moon.png";
 
 const Destination = () => {
   return (
     <>
       <StlSection>
         <SectionContainer>
-          <H5>01 Pick your destination</H5>
-          <nav>
+          <ContainerHeader>
+            <H5>
+              <span>01 </span>Pick your destination
+            </H5>
+            <HeaderImage>
+              <img src={moon} alt="Moon Picture" />
+            </HeaderImage>
             <ul>
               <li>Moon</li>
               <li>Mars</li>
               <li>Europa</li>
               <li>Titan</li>
             </ul>
-          </nav>
-          <H2>Moon</H2>
-          <p>
-            See our planet as you’ve never seen it before. A perfect relaxing
-            trip away to help regain perspective and come back refreshed. While
-            you’re there, take in some history by visiting the Luna 2 and Apollo
-            11 landing sites.
-          </p>
-          <hr />
-          <div className="sub-title">Avg. distance</div>
-          <div className="sub-title">384,400 km Est.</div>
-          <div className="sub-title">travel time </div>
-          <div className="sub-title">3 days</div>
+          </ContainerHeader>
+          <ContainerBody>
+            <H2>Moon</H2>
+            <p>
+              See our planet as you’ve never seen it before. A perfect relaxing
+              trip away to help regain perspective and come back refreshed.
+              While you’re there, take in some history by visiting the Luna 2
+              and Apollo 11 landing sites.
+            </p>
+            <hr />
+
+            <SubHeading>
+              <div>
+                <SubH2>Avg. distance</SubH2>
+                <SubH1>384,400 km</SubH1>
+              </div>
+              <div>
+                <SubH2>Est. travel time </SubH2>
+                <SubH1>3 days</SubH1>
+              </div>
+            </SubHeading>
+          </ContainerBody>
         </SectionContainer>
       </StlSection>
     </>
--- src/components/Templates/Destination/styles.ts
+++ src/components/Templates/Destination/styles.ts
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
-import { images } from "../../Global/vars";
+import { colors, images } from "../../Global/vars";
 
 const StlSection = styled.section`
   background: url(${images.destinationMobile}) no-repeat;
   background-size: cover;
   width: 100%;
   height: 100vh;
-  `;
+`;
 
 const SectionContainer = styled.div`
   width: 100%;
@@ -16,7 +16,67 @@ const SectionContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-between;
 `;
 
-export { StlSection, SectionContainer }
+const ContainerHeader = styled.div`
+  h5 {
+    color: ${colors.white};
+    padding-bottom: 0.375rem;
+    span {
+      color: rgba(255, 255, 255, 0.3);
+      font-weight: 700;
+    }
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1.625rem;
+    text-transform: uppercase;
+    letter-spacing: 0.148rem;
+  }
+    
+`;
+
+const HeaderImage = styled.div`
+  width: 100%;
+  max-width: 10.625rem;
+  height: 100%;
+  max-height: 10.625rem;
+  margin: 1.625rem auto;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+`;
+
+const ContainerBody = styled.div`
+  padding-top: 2rem;
+
+  hr {
+    margin: 2rem 0;
+    border-color: ${colors.charcoal};
+  }
+`;
+
+
+const SubHeading = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  
+  div {
+    display: flex;
+    flex-direction: column;
+    
+    gap: 0.75rem;
+  }
+`;
+
+
+export { StlSection, SectionContainer, ContainerHeader, HeaderImage, ContainerBody, SubHeading }
