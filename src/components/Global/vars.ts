08c2e64a0a66e77dcbbda864fdec329e0191f5ed
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-05 11:06:05 -0300

    Feat Home: Remove Unused space

--- src/components/Global/vars.ts
+++ src/components/Global/vars.ts
@@ -1,7 +1,6 @@
 import homeMobileBackground from '../../assets/home/background-home-mobile.jpg';
 import homeTabletBackground from '../../assets/home/background-home-tablet.jpg';
 import homeDesktopBackground from '../../assets/home/background-home-desktop.jpg';
-
 import hamburgerMenu from '../../assets/shared/icon-hamburger.svg';
 
 const colors = {
