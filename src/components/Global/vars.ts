ea9856ebb33e762d552c08a349c2eea943fd6176
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-05 12:56:09 -0300

    Feat Destination: Add Destination background to mobile, tablet and desktop

--- src/components/Global/vars.ts
+++ src/components/Global/vars.ts
@@ -1,8 +1,15 @@
+// Images
+import hamburgerMenu from '../../assets/shared/icon-hamburger.svg';
+
 import homeMobileBackground from '../../assets/home/background-home-mobile.jpg';
 import homeTabletBackground from '../../assets/home/background-home-tablet.jpg';
 import homeDesktopBackground from '../../assets/home/background-home-desktop.jpg';
-import hamburgerMenu from '../../assets/shared/icon-hamburger.svg';
 
+import destinationMobileBackground from '../../assets/destination/background-destination-mobile.jpg';
+import destinationTabletBackground from '../../assets/destination/background-destination-tablet.jpg';
+import destinationDesktopBackground from '../../assets/destination/background-destination-desktop.jpg';
+
+// Colors
 const colors = {
   richBlack: "#0B0D17",
   periwinkle: "#D0D6F9",
@@ -14,6 +21,9 @@ const images = {
   homeMobile: homeMobileBackground,
   homeTablet: homeTabletBackground,
   homeDesktop: homeDesktopBackground,
+  destinationMobile: destinationMobileBackground,
+  destinationTablet: destinationTabletBackground,
+  destinationDesktop: destinationDesktopBackground,
 }
 
 export { colors, images };
