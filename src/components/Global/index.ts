f91d6131301fb420810a0209845dee607bea2904
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-05 11:03:32 -0300

    Feat Home: Add Global Styles

--- src/components/Global/index.ts
+++ src/components/Global/index.ts
@@ -15,6 +15,8 @@ const GlobalStyle = createGlobalStyle`
     font-family: "Barlow Condensed", sans-serif;
     color: ${colors.white};
     font-size: 1rem;
+    font-weight: 400;
+    font-style: normal;
   }
 
   body {
@@ -23,13 +25,42 @@ const GlobalStyle = createGlobalStyle`
     position: relative;
   }
 
-  h1, h2, h3, h4 {
-    font-family: "Bellefair", serif;
+  main {
+    width: 100%;
+    height: 100%;
+    text-align: center;
   }
 
+  h1, h2, h3, h4, button {
+    font-family: "Bellefair", serif;
+    font-weight: 400;
+    font-style: normal;
+    text-transform: uppercase;
+  }
+  
   ul, ol {
     list-style: none;
   }
+  
+  p {
+    color: ${colors.periwinkle};
+    font-size: 0.938rem;
+    line-height: 1.563rem;
+
+    /* font-size: 1rem; */
+
+    /* font-size: 1.125rem; */
+    /* line-height: 1.75rem; */
+    
+    /* line-height: 2rem; */
+  }
+
+  a {
+    text-decoration: none;
+    text-transform: uppercase;
+    color: ${colors.white};
+  }
+
 `;
 
 export default GlobalStyle
