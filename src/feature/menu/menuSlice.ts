ee655adf6b245d79437e4ecc99364715dbbc83d1
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-07 10:02:58 -0300

    Feature Menu: Create menu slice to open and close Aside component

---
+++ src/feature/menu/menuSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type TInitialState = {
+  isOpen: boolean;
+}
+
+const initialState: TInitialState = {
+  isOpen: false,
+}
+
+export const menuSlice = createSlice({
+  name: 'menu',
+  initialState,
+  reducers: {
+    open: (state) => {
+      state.isOpen = true;
+    },
+    close: (state) => {
+      state.isOpen = false;
+    },
+  }
+});
+
+export const {
+  open,
+  close
+} = menuSlice.actions;
+
+export default menuSlice.reducer;
