9bbac1a901043a5f080cc300f829e9dec0d11d6f
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-07 09:56:11 -0300

    Feature Menu: Add New Aside Component

--- src/components/Templates/Root/index.tsx
+++ src/components/Templates/Root/index.tsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
+
+import { useAppSelector } from "../../../store/hooks";
+
 import Header from "../../Organisms/Header";
+import Aside from "../../Organisms/Aside";
 
 const Root = () => {
+  const isOpen = useAppSelector(state => state.menu.isOpen)
   return (
     <>
       <Header />
+      <Aside isOpen={isOpen} />
       <main>
         <Outlet />
       </main>
