e436e40550848f4fcd0c65a4bb7fffa3a3fa33f7
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-07 10:21:47 -0300

    Feature Menu: Add onClick action to Mobile Menu

--- src/components/Molecules/Nav/AsideNav/index.tsx
+++ src/components/Molecules/Nav/AsideNav/index.tsx
@@ -1,14 +1,22 @@
 import { Anchor } from "../../../Atoms";
 import { StlNav, AsideList } from "./styles";
 import { menuLinks } from "../menu.data";
+import { useAppDispatch } from "../../../../store/hooks";
+import { close } from "../../../../feature/menu/menuSlice";
+
 
 const AsideNav = () => {
+  const dispatch = useAppDispatch();
+  const handleClick = () => {
+    dispatch(close());
+  }
+
   return (
     <StlNav>
       <AsideList>
         {menuLinks.map((menuLink) => (
           <li key={menuLink.title}>
-            <Anchor to={menuLink.href}>
+            <Anchor to={menuLink.href} onClick={handleClick}>
               <strong>{menuLink.number} </strong>
               {menuLink.title}
             </Anchor>
