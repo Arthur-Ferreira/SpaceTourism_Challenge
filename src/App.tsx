5cf95f97752d390d0875f23a5c9cd62fb234b9fd
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-07 22:59:01 -0300

    Feat App: RouterProvider update with new routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,22 @@ import GlobalStyle from "./components/Global";
 import Destination from "./components/Templates/Destination";
 import Crew from "./components/Templates/Crew";
 import Technology from "./components/Templates/Technology";
+import DestinationArticle from "./components/Organisms/CrewArticle";
+import CrewArticle from "./components/Organisms/CrewArticle";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route index element={<Home />} />
-      <Route path="destination" element={<Destination />} />
-      <Route path="crew" element={<Crew />} />
-      <Route path="technology" element={<Technology />} />
+      <Route path="destination" element={<Destination />}>
+        <Route path=":name" element={<DestinationArticle />} />
+      </Route>
+      <Route path="crew" element={<Crew />}>
+        <Route path=":name" element={<CrewArticle />} />
+      </Route>
+      <Route path="technology" element={<Technology />}>
+        {/* <Route path=":name" element={<DestinationArticle />} /> */}
+      </Route>
     </Route>
   )
 );
