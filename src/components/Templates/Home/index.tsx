6cdf46bb143cbccba8ec75d87f52b9eef000d4d8
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-07 22:56:11 -0300

    Feat Destinations: Link to Destinations Page update

--- src/components/Templates/Home/index.tsx
+++ src/components/Templates/Home/index.tsx
@@ -6,7 +6,7 @@ import { SectionContainer, StlSection } from "./styles";
 const Home = () => {
   const navigate = useNavigate()
   function handleNavigate() {
-    navigate("/destination")
+    navigate("/destination/moon")
   }
 
   return (
