97e4ad1c77d0c5aa1a3272923d9f9d0a8f6775d8
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-07 22:58:14 -0300

    Feat Crew: Template update with new Crew Article component

--- src/components/Templates/Crew/index.tsx
+++ src/components/Templates/Crew/index.tsx
@@ -1,44 +1,13 @@
-import { H3, H4, H5 } from "../../Atoms";
-import {
-  StlSection,
-  SectionContainer,
-  ContainerHeader,
-  HeaderImage,
-  Titles,
-} from "./styles";
-import commander from "../../../assets/crew/image-douglas-hurley.png";
+import { StlSection, SectionContainer } from "./styles";
+
+import CrewArticle from "../../Organisms/CrewArticle";
 
 const Crew = () => {
   return (
     <>
       <StlSection>
         <SectionContainer>
-          <ContainerHeader>
-            <H5>
-              <span>02 </span>Meet your crew
-            </H5>
-            <HeaderImage>
-              <img src={commander} alt="Commander - Douglas Hurley" />
-            </HeaderImage>
-            <hr />
-          </ContainerHeader>
-          <div>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-            <Titles>
-              <H4>Commander</H4>
-              <H3>Douglas Hurley</H3>
-            </Titles>
-            <p>
-              Douglas Gerald Hurley is an American engineer, former Marine Corps
-              pilot and former NASA astronaut. He launched into space for the
-              third time as commander of Crew Dragon Demo-2.
-            </p>
-          </div>
+          <CrewArticle />
         </SectionContainer>
       </StlSection>
     </>
--- src/components/Templates/Crew/styles.ts
+++ src/components/Templates/Crew/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { colors, images } from "../../Global/vars";
+import { images } from "../../Global/vars";
 
 const StlSection = styled.section`
   background: url(${images.crewMobile}) no-repeat;
@@ -13,62 +13,7 @@ const SectionContainer = styled.div`
   width: 100%;
   height: 100%;
   padding: 5.5rem 1.5rem 3.625rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  `;
-
-const ContainerHeader = styled.div`
-  width: 100%;
-  h5 {
-    color: ${colors.white};
-    padding-bottom: 0.375rem;
-    span {
-      opacity: 0.3;
-    }
-  }
-
-  hr {
-    margin-bottom: 2rem;
-    border-color: ${colors.charcoal};
-  }
-  
-  ul {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 1.625rem;
-    text-transform: uppercase;
-    letter-spacing: 0.148rem;
-  }
-`;
-
-const HeaderImage = styled.div`
-  width: 100%;
-  max-width: 11.07rem;
-  height: 100%;
-  max-height: 13.875rem;
-  margin: 2rem auto 0;
-
-  img {
-    width: 100%;
-    height: 100%;
-  }
-
-`;
-
-const Titles = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 0.5rem;
-  margin: 1rem 0;
-  
-  h4 {
-    opacity: 0.5;
-  }
-
 `;
 
 
-export { StlSection, SectionContainer, ContainerHeader, HeaderImage, Titles }
+export { StlSection, SectionContainer }
