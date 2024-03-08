d11447a87641b9952c95f2b0eec500cfdcf9fcc7
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-08 00:47:57 -0300

    Feat Technology: Technology Template update to handle new Technology Article

--- src/components/Templates/Technology/index.tsx
+++ src/components/Templates/Technology/index.tsx
@@ -1,45 +1,12 @@
-import { H3, H5 } from "../../Atoms";
-import {
-  StlSection,
-  SectionContainer,
-  ContainerHeader,
-  HeaderImage,
-  ContainerBody,
-  Titles
-} from "./styles";
-import rocket from "../../../assets/technology/image-launch-vehicle-landscape.jpg";
+import { StlSection, SectionContainer } from "./styles";
+import TechnologyArticle from "../../Organisms/TechnologyArticle";
 
 const Crew = () => {
   return (
     <>
       <StlSection>
         <SectionContainer>
-          <ContainerHeader>
-            <H5>
-              <span>03 </span>Space launch 101
-            </H5>
-            <HeaderImage>
-              <img src={rocket} alt="Space launch 101" />
-            </HeaderImage>
-          </ContainerHeader>
-          <ContainerBody>
-            <ul>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-            </ul>
-            <Titles>
-              <H5>The terminology...</H5>
-              <H3>Launch vehicle</H3>
-            </Titles>
-            <p>
-              A launch vehicle or carrier rocket is a rocket-propelled vehicle
-              used to carry a payload from Earth's surface to space, usually to
-              Earth orbit or beyond. Our WEB-X carrier rocket is the most
-              powerful in operation. Standing 150 metres tall, it's quite an
-              awe-inspiring sight on the launch pad!
-            </p>
-          </ContainerBody>
+          <TechnologyArticle />
         </SectionContainer>
       </StlSection>
     </>
