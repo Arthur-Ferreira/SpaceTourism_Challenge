e223c003d26e83b7ec348b4f13f85c6020f1e200
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-11 16:30:51 -0300

    Feat App: Change BrowserRouter to the right routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,20 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import Home from "./components/Templates/Home";
 import Root from "./components/Templates/Root";
+import Home from "./components/Templates/Home";
 import GlobalStyle from "./components/Global";
 import Destination from "./components/Templates/Destination";
 import Crew from "./components/Templates/Crew";
 import Technology from "./components/Templates/Technology";
-import DestinationArticle from "./components/Organisms/CrewArticle";
-import CrewArticle from "./components/Organisms/CrewArticle";
-import TechnologyArticle from "./components/Organisms/TechnologyArticle";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route index element={<Home />} />
-      <Route path="destination" element={<Destination />}>
-        <Route path=":name" element={<DestinationArticle />} />
-      </Route>
-      <Route path="crew" element={<Crew />}>
-        <Route path=":name" element={<CrewArticle />} />
-      </Route>
-      <Route path="technology" element={<Technology />}>
-        <Route path=":name" element={<TechnologyArticle />} />
-      </Route>
+      <Route path="destinations" element={<Destination />} />
+      <Route path="crew" element={<Crew />} />
+      <Route path="technology" element={<Technology />} />
     </Route>
   )
 );
