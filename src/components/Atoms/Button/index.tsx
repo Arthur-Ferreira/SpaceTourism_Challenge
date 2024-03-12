138851f056ae0ed586ebc87b6f865082a59c456e
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-12 11:22:24 -0300

    Feat App: Create new Button to match with layout

--- src/components/Atoms/Button/index.tsx
+++ src/components/Atoms/Button/index.tsx
@@ -1,16 +1,43 @@
 import type React from "react";
 
-import StlButton from "./styles";
+import { StlButton, StlDestButton, StlCrewButton, StlTechButton } from "./styles";
 
 interface IButton {
   type: "button" | "reset" | "submit" | undefined;
   className?: string;
   onClick?: () => void;
-  children: React.ReactNode;
+  children?: React.ReactNode;
 }
 
-const Button: React.FC<IButton> = ({ type, className, onClick,children }) => {
-  return <StlButton type={type} className={className} onClick={onClick}>{children}</StlButton>;
+const Button: React.FC<IButton> = ({ type, className, onClick, children }) => {
+  return (
+    <StlButton type={type} className={className} onClick={onClick}>
+      {children}
+    </StlButton>
+  );
 };
 
-export default Button;
+const DestButton: React.FC<IButton> = ({ type, onClick, children }) => {
+  return (
+    <StlDestButton type={type} onClick={onClick}>
+      {children}
+    </StlDestButton>
+  );
+};
+
+const CrewButton: React.FC<IButton> = ({ type, onClick }) => {
+  return (
+    <StlCrewButton type={type} onClick={onClick}>
+    </StlCrewButton>
+  );
+};
+
+const TechButton: React.FC<IButton> = ({ type, onClick, children }) => {
+  return (
+    <StlTechButton type={type} onClick={onClick}>
+      {children}
+    </StlTechButton>
+  );
+};
+
+export { Button, DestButton, CrewButton, TechButton };
--- src/components/Atoms/Button/styles.ts
+++ src/components/Atoms/Button/styles.ts
@@ -27,4 +27,56 @@ const StlButton = styled.button`
   }
 `;
 
-export default StlButton;
+const StlDestButton = styled.button`
+  font-family: "Barlow Condensed", sans-serif;
+  color: ${colors.white};
+  font-size: 1rem;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: 0.169rem;
+  text-transform: uppercase;
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  border-bottom: 4px solid transparent;
+  padding-bottom: 0.5rem;
+
+  &:hover {
+    border-bottom: 4px solid ${colors.white};
+  }
+`;
+
+const StlCrewButton = styled.button`
+  cursor: pointer;
+  background: ${colors.white};
+  opacity: 0.3;
+  border: none;
+  padding: 0.3rem;
+  border-radius: 50%;
+  
+  &:hover {
+    opacity: 1;
+  }
+  `;
+
+const StlTechButton = styled.button`
+  color: ${colors.white};
+  font-size: 1rem;
+  font-weight: 400;
+  font-style: normal;
+  cursor: pointer;
+  text-transform: uppercase;
+  letter-spacing: 0.169rem;
+  background: transparent;
+  border: 1px solid ${colors.white};
+  border-radius: 50%;
+  padding: 0.6rem 1rem;
+
+  &:hover {
+    color: ${colors.richBlack};
+    background-color: ${colors.white};
+  }
+`;
+
+
+export { StlButton, StlDestButton, StlCrewButton, StlTechButton };
